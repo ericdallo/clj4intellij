032f069c8fbbbbabfed2a73267cf3a9bbbd2eaf2
André Camargo <andre.camargo@nubank.com.br>
2025-02-11 13:23:04 -0300

    Workaround clj-kondo bad analysis of clojure.core/proxy using reify

--- resources/clj-kondo.exports/nubank/proxy-plus/nubank/proxy_plus.clj
+++ resources/clj-kondo.exports/nubank/proxy-plus/nubank/proxy_plus.clj
@@ -1,33 +1,65 @@
 (ns nubank.proxy-plus
-    (:require [clj-kondo.hooks-api :as hooks]))
+  (:require [clj-kondo.hooks-api :as hooks]))
 
 (defn proxy+
-      [{{:keys [children]} :node :as _context}]
-      (binding [*out* *err*] (println 'proxy+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++))
-      (if (hooks/vector-node? (second children))
-          (let [[_proxy+ super-args-node & impl-body-nodes] children
-                new-node (hooks/list-node (concat [(hooks/token-node 'clojure.core/proxy)
-                                                   (hooks/vector-node [(hooks/token-node '_ClassNameSymbol)])
-                                                   super-args-node]
-                                                  impl-body-nodes))]
-               {:node new-node})
-          (let [[_proxy+ class-name-symbol super-args & impl-body] children
-                new-node (hooks/list-node (concat [(hooks/token-node 'clojure.core/proxy)
-                                                   (hooks/vector-node [class-name-symbol])
-                                                   super-args]
-                                                  impl-body))]
-               {:node new-node})))
+  "Expands a proxy+ form into a reify form with a declared proxy name.
+
+   Input:
+     (proxy+
+       [^String title ^String description ^Icon icon]
+       DumbAwareAction
+        (actionPerformed [_ event] (on-performed event)))
+   Output:
+     (do
+       (clojure.core/declare proxy_plus14738)
+       (clojure.core/let [_ [title description icon]]
+         (clojure.core/reify
+           DumbAwareAction
+           (actionPerformed [_ event] (on-performed event)))))
+   "
+  [{{:keys [children]} :node :as _context}]
+  (let [[_proxy+ & args] children
+        [proxy-name-sym super-args impls]
+        (if (symbol? (hooks/sexpr (first args)))
+          [(first args)
+           (first (rest args))
+           (rest (rest args))]
+          [(gensym "proxy_plus")
+           (first args)
+           (rest args)])
+        new-node (hooks/list-node
+                  [(hooks/token-node 'do)
+                   (hooks/list-node
+                    [(hooks/token-node 'clojure.core/declare)
+                     proxy-name-sym])
+                   (hooks/list-node
+                    [(hooks/token-node 'clojure.core/let)
+                     (hooks/vector-node [(hooks/token-node '_)
+                                         super-args])
+                     (hooks/list-node
+                      (concat [(hooks/token-node 'clojure.core/reify)]
+                              impls))])])]
+    {:node new-node
+     :defined-by 'com.rpl.proxy-plus/proxy+}))
 
 (comment
- (require '[clj-kondo.hooks-api :as hooks])
+  ;; (require '[clojure.repl.deps :as repl])
+  ;; (repl/add-lib 'clj-kondo/clj-kondo {:mvn/version "2025.01.16"})
+  ;; I'd love if this worked, but it's returning:
+  ;; ; Syntax error (IllegalArgumentException) compiling fn* at (clj_kondo/impl/analysis/java.clj:156:1).
+  ;; ; Error - no matches found for static method ASM9 in class org.objectweb.asm.Opcodes
+  ;; So, you need to add clj-kondo to the deps.edn file in order to evaluate
+  ;; the following code:
+  (require '[clj-kondo.hooks-api :as hooks])
 
- (->> "(proxy+
+  (->> "(proxy+
          [^String title ^String description ^Icon icon]
          DumbAwareAction
           (actionPerformed [_ event] (on-performed event)))"
-      hooks/parse-string
-      (assoc {} :node)
-      proxy+
-      :node
-      str)
- )
+       hooks/parse-string
+       (assoc {} :node)
+       proxy+
+       :node
+       str)
+  ;; => "(do (clojure.core/declare proxy_plus14738) (clojure.core/let [_ [title description icon]] (clojure.core/reify DumbAwareAction (actionPerformed [_ event] (on-performed event)))))"
+  )
